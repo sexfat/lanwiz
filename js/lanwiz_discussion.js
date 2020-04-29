ff7d682544795ce40766bc7f38e7e80524d0d57a
Ching-Lin523 <62847465+Ching-Lin523@users.noreply.github.com>
2020-04-29 18:35:10 +0800

    rwd and index-review update

---
+++ js/lanwiz_discussion.js
@@ -0,0 +1,13 @@
+document.getElementById('menubtn').addEventListener('click', function (e) {
+    $(this).toggleClass('active');
+    $('#menuUl').toggleClass('active');
+})
+
+$('#menuUl li').click(function () {
+    $(this).parent().find('li').each(function () {
+        if ($(this).hasClass('current-active')) {
+            $(this).toggleClass('current-active');
+        }
+    })
+    $(this).toggleClass('current-active');
+})
