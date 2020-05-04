af2bcfdd32aa6ba59859556bd3cf5c2c1d217205
sexfat <bryant6831@gmail.com>
2020-05-04 23:05:59 +0800

    add

--- js/tab.js
+++ js/tab.js
@@ -1,5 +1,4 @@
-
-  $(function () {
+$(function () {
     // tab 切換的
     // var video, intro;
 
@@ -10,22 +9,21 @@
 
 
     // video 的按鈕 底下內容要出現
-    video.on("click",function(e){
-      e.preventDefault();
-      var current_fs = $(this).parent().parent().parent();
-      var next_fs = $(this).parent().parent().parent();
-      current_fs.find('.tabContainer:first').css("display", "block");
-      next_fs.find('.tabContainer:last').css("display", "none");
-      
+    video.on("click", function (e) {
+        e.preventDefault();
+        var current_fs = $(this).parent().parent().parent();
+        var next_fs = $(this).parent().parent().parent();
+        current_fs.find('.tabContainer:first').css("display", "block");
+        next_fs.find('.tabContainer:last').css("display", "none");
+
     });
     // Introduction 的按鈕 底下內容要出現
-    intro.on("click",function(e){
-      e.preventDefault();
-      var current_fs = $(this).parent().parent().parent();
-      var next_fs = $(this).parent().parent().parent();
-      current_fs.find('.tabContainer:first').css("display", "none");
-      next_fs.find('.tabContainer:last').css("display", "block");
+    intro.on("click", function (e) {
+        e.preventDefault();
+        var current_fs = $(this).parent().parent().parent();
+        var next_fs = $(this).parent().parent().parent();
+        current_fs.find('.tabContainer:first').css("display", "none");
+        next_fs.find('.tabContainer:last').css("display", "block");
     });
 
-  });
-
+});
